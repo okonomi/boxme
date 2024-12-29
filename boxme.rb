567a376200739b619ce885ecdca1ce92ba240f39
okonomi <okonomi@oknm.jp>
2024-12-29 21:56:05 +0900

    feat: use OAuth2 authorization URL

--- boxme.rb
+++ boxme.rb
@@ -1,4 +1,5 @@
 require "bundler/setup"
+require "securerandom"
 Bundler.require(:default)
 
 case ARGV[0]
@@ -11,7 +12,13 @@ when "login"
   print "Enter your client secret: "
   client_secret = STDIN.gets.chomp
 
-  Launchy.open("https://box.com")
+  base_url = "https://app.box.com/api/oauth2/authorize"
+  redirect_uri = "http://localhost:3000/callback"
+  response_type = "code"
+  state = SecureRandom.hex(32)
+  authorization_url = "#{base_url}?client_id=#{client_id}&response_type=#{response_type}&state=#{state}&redirect_uri=#{redirect_uri}"
+
+  Launchy.open(authorization_url)
 when "user"
   puts "Show user information"
 else
