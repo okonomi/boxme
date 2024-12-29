32720d4bcd8ba1e15256bb2be5cdc369acb241e8
okonomi <okonomi@oknm.jp>
2024-12-29 13:37:38 +0900

    feat: add client ID and secret input for login

--- boxme.rb
+++ boxme.rb
@@ -3,6 +3,12 @@ require "bundler/setup"
 case ARGV[0]
 when "login"
   puts "Login to Box"
+
+  print "Enter your client ID: "
+  client_id = STDIN.gets.chomp
+
+  print "Enter your client secret: "
+  client_secret = STDIN.gets.chomp
 when "user"
   puts "Show user information"
 else
