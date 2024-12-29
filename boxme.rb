ea60d0e144159aba876bc840087ca118ac64d4a0
okonomi <okonomi@oknm.jp>
2024-12-29 22:59:41 +0900

    feat: implement basic TCP server for authorization callback

--- boxme.rb
+++ boxme.rb
@@ -1,6 +1,36 @@
 require "bundler/setup"
-require "securerandom"
 Bundler.require(:default)
+
+require "securerandom"
+require "socket"
+
+module Boxme
+  class Server
+    def start
+      server = TCPServer.new 3000
+      while session = server.accept
+        request = session.gets
+        puts request
+
+        response = <<~RESPONSE
+          HTTP/1.1 200 OK
+          Content-Type: text/html
+
+          <html>
+            <body>
+              <h1>Boxme</h1>
+              <p>Authorization successful</p>
+            </body>
+          </html>
+        RESPONSE
+
+        session.puts response
+        session.close
+        return
+      end
+    end
+  end
+end
 
 case ARGV[0]
 when "login"
@@ -12,6 +42,10 @@ when "login"
   print "Enter your client secret: "
   client_secret = STDIN.gets.chomp
 
+  server = Thread.new do
+    Boxme::Server.new.start
+  end
+
   base_url = "https://app.box.com/api/oauth2/authorize"
   redirect_uri = "http://localhost:3000/callback"
   response_type = "code"
@@ -19,6 +53,8 @@ when "login"
   authorization_url = "#{base_url}?client_id=#{client_id}&response_type=#{response_type}&state=#{state}&redirect_uri=#{redirect_uri}"
 
   Launchy.open(authorization_url)
+
+  server.join
 when "user"
   puts "Show user information"
 else
