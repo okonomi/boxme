dff02ba150ac75d70b6bc2469a16a89f556fcc13
okonomi <okonomi@oknm.jp>
2024-12-30 16:52:34 +0900

    feat: retrieve and display current user information

--- lib/boxme/commands/user.rb
+++ lib/boxme/commands/user.rb
@@ -1,8 +1,43 @@
+# frozen_string_literal: true
+
+require "net/http"
+require "uri"
+require "json"
+require "xdg"
+
 module Boxme
   class Commands
     class User
+      USER_URL = URI("https://api.box.com/2.0/users/me").freeze
+
       def call
         puts "Show user information"
+
+        token_info = load_token_info
+        p token_info
+
+        get_current_user(token_info)
+      end
+
+      private
+
+      def load_token_info
+        path = XDG::Config.new.home.join("boxme", "token.json")
+        token_info = JSON.load(path.read) if path.file?
+
+        raise "No token found. Please login first" unless token_info
+
+        token_info
+      end
+
+      def get_current_user(token_info)
+        http = Net::HTTP.new(USER_URL.host, USER_URL.port)
+        http.use_ssl = USER_URL.scheme === "https"
+
+        headers = { authorization: "Bearer #{token_info["access_token"]}" }
+        response = http.get(USER_URL.path, headers)
+
+        pp JSON.parse(response.body)
       end
     end
   end
